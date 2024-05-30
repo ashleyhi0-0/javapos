c77ce97a0bf42e74c9761b4075c6d3ffb0a5c957
sgfrdgrln <gazel0123@gmail.com>
2024-05-30 11:36:57 +0800

    category

--- app/src/main/java/com/example/java_pos/Views/MainActivity.java
+++ app/src/main/java/com/example/java_pos/Views/MainActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.java_pos.Models.Category;
 import com.example.java_pos.R;
 import com.example.java_pos.Repo.Adapter.CategoryAdapter;
 import com.example.java_pos.Repo.CategoryRepo;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +20,37 @@ public class MainActivity extends AppCompatActivity {
     CategoryAdapter categoryAdapter;
     RecyclerView recyclerView;
 
+    List<Category> categories;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.category_activity);
+
+        categoryRepo = new CategoryRepo();
+
+        categoryList();
+
+
+    }
+
+    private void categoryList() {
+
+        recyclerView = findViewById(R.id.category_recview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        fetchAndDisplayCategory();
+    }
+
+    private void fetchAndDisplayCategory() {
+
+        categories = categoryRepo.getAllCategories();
+
+        categoryAdapter = new CategoryAdapter(categories, this);
+
+        recyclerView.setAdapter(categoryAdapter);
+
     }
 }
